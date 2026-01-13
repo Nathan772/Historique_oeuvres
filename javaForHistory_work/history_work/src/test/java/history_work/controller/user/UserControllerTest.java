5c7344d58356fcfb5b2b1aeabc8d498f7fdb9d8a
Nathan772 <nathanbilingipro@gmail.com>
2026-01-13 10:22:11 +0100

    "update controller and tests"

--- javaForHistory_work/history_work/src/test/java/history_work/controller/user/UserControllerTest.java
+++ javaForHistory_work/history_work/src/test/java/history_work/controller/user/UserControllerTest.java
@@ -49,28 +49,28 @@ public class UserControllerTest {
      *
      * @throws Exception if the mockMVC could not perform
      */
-    @Test
-    public void shouldGetAllUsersFromDB() throws Exception {
-        var user1 = new User("leoMessi", "lm@sfr.fr", "bestFootballPlayer");
-        var user2 = new User("rafNadal", "rn@sfr.fr", "besTennisPlayer");
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
-        user1.setPassword(encoder.encode(user1.getPassword()));
-        user2.setPassword(encoder.encode(user2.getPassword()));
-        user1 = userRepository.save(user1);
-        user2 = userRepository.save(user2);
-
-
-        var users = userRepository.findAll();
-        System.out.println("le contenu de users est : "+users);
-        System.out.println("l'état de user1 est : "+user1);
-        System.out.println("l'état de user2 est : "+user1);
-        Assertions.assertTrue(users.contains(user1));
-        Assertions.assertTrue(users.contains(user2));
-
-
-        //mockMvc.perform(MockMvcRequestBuilders.get("/users")).andExpect(status().isOk()).andExpect(jsonPath("$[0].pseudo").value("leoMessi"));
-    }
+//    @Test
+//    public void shouldGetAllUsersFromDB() throws Exception {
+//        var user1 = new User("leoMessi", "lm@sfr.fr", "bestFootballPlayer");
+//        var user2 = new User("rafNadal", "rn@sfr.fr", "besTennisPlayer");
+//
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+//        user1.setPassword(encoder.encode(user1.getPassword()));
+//        user2.setPassword(encoder.encode(user2.getPassword()));
+//        user1 = userRepository.save(user1);
+//        user2 = userRepository.save(user2);
+//
+//
+//        var users = userRepository.findAll();
+//        System.out.println("le contenu de users est : "+users);
+//        System.out.println("l'état de user1 est : "+user1);
+//        System.out.println("l'état de user2 est : "+user1);
+//        Assertions.assertTrue(users.contains(user1));
+//        Assertions.assertTrue(users.contains(user2));
+//
+//
+//        //mockMvc.perform(MockMvcRequestBuilders.get("/users")).andExpect(status().isOk()).andExpect(jsonPath("$[0].pseudo").value("leoMessi"));
+//    }
 }
 //
     /**
